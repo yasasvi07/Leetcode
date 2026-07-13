ab4e0f5cc84ab3149a31b77fd1403edd1a29848d
yasasvi07 <160727492+yasasvi07@users.noreply.github.com>
2026-07-13 13:16:00 +0530

    Time: 1 ms (28.06%), Space: 42.8 MB (6.32%) - LeetHub

--- 1291-sequential-digits/1291-sequential-digits.java
+++ 1291-sequential-digits/1291-sequential-digits.java
@@ -3,7 +3,6 @@ class Solution {
         List<Integer> ans = new ArrayList<>();
         String s = "123456789";
         StringBuilder sb = new StringBuilder();
-        HashSet<Integer> hs = new HashSet<>();
         for(int i=0;i<s.length();i++)
         {
             sb.append(s.charAt(i));
@@ -12,13 +11,11 @@ class Solution {
                 sb.append(s.charAt(j));
                 if(Integer.parseInt(sb.toString())<=high && Integer.parseInt(sb.toString())>=low)
                 {
-                    hs.add(Integer.parseInt(sb.toString()));
+                    ans.add(Integer.parseInt(sb.toString()));
                 }
             }
             sb.setLength(0);
         }
-        for(int i:hs)
-        ans.add(i);
         Collections.sort(ans);
         return ans;
     }
