af1318c5724758302b0a0357fbd717281146e779
yasasvi07 <160727492+yasasvi07@users.noreply.github.com>
2026-08-19 15:38:28 +0530

    Time: 31 ms (38.16%), Space: 56.9 MB (16.32%) - LeetHub

---
+++ 1386-cinema-seat-allocation/1386-cinema-seat-allocation.java
@@ -0,0 +1,47 @@
+class Solution {
+    public int maxNumberOfFamilies(int n, int[][] reservedSeats) {
+        HashMap<Integer,HashSet<Integer>> hm = new HashMap<>();
+        for(int[] i:reservedSeats)
+        {
+            hm.putIfAbsent(i[0]-1,new HashSet<>());
+            hm.get(i[0]-1).add(i[1]-1);
+        }
+        int cnt = (n-hm.size())*2;
+        for(Map.Entry<Integer,HashSet<Integer>> entry:hm.entrySet())
+        {
+            boolean left = true;
+            boolean middle = true;
+            boolean right = true;
+            Set<Integer> hs = entry.getValue();
+            for(int j=1;j<5;j++)
+            {
+                if(hs!=null && hs.contains(j))
+                {
+                    left = false;
+                    break;
+                }
+            }
+            for(int j=3;j<7;j++)
+            {
+                if(hs!=null && hs.contains(j))
+                {
+                    middle = false;
+                    break;
+                }
+            }
+            for(int j=5;j<9;j++)
+            {
+                if(hs!=null && hs.contains(j))
+                {
+                    right = false;
+                    break;
+                }
+            }
+            if(left && right)
+            cnt+=2;
+            else if(left || middle || right)
+            cnt++;
+        }
+        return cnt;
+    }
+}
