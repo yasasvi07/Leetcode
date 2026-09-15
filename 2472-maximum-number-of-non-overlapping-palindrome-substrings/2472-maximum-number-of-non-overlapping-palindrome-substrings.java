82a945f9d4c53a2c91a4f9d5ae265c086a1c7311
yasasvi07 <160727492+yasasvi07@users.noreply.github.com>
2026-09-15 12:19:20 +0530

    Time: 1 ms (100%), Space: 42.6 MB (94.97%) - LeetHub

--- 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.java
+++ 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.java
@@ -14,23 +14,20 @@ class Solution {
         int n = s.length();
         if(k==1)
         return n;
-        int[] dp = new int[n+1];
-        for(int i=0;i<n;i++)
+        int ans = 0;
+        for(int i=0;i<=n-k;i++)
         {
-            dp[i+1] = Math.max(dp[i+1],dp[i]);
-            //StringBuilder sb = new StringBuilder();
-            for(int j=i;j<n;j++)
+            if(palindrome(s,i,i+k-1))
             {
-                //sb.append(s.charAt(j));
-                if(j-i+1>=k)
-                {
-                    if(palindrome(s,i,j))
-                    {
-                        dp[j+1] = Math.max(dp[j+1],dp[i]+1);
-                    }
-                }
+                ans++;
+                i+=k-1;
+            }
+            else if(i<n-k && palindrome(s,i,i+k))
+            {
+                ans++;
+                i+=k;
             }
         }
-        return dp[n];
+        return ans;
     }
 }
